5097b3cbf687a0b3e2a4b46da59dcc18d765ef0b
mertcankirci <mertkirci02@gmail.com>
2023-04-16 15:13:08 +0300

    Project created

---
+++ RickAndMorty/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Mertcan Kırcı on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
